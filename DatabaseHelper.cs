85d7ccb90ebf07fbd7651284e0170767504b89e2
Dark King <121280699+dark-king-07@users.noreply.github.com>
2024-10-03 01:19:18 +0530

    Update DatabaseHelper.cs
    .

--- DatabaseHelper.cs
+++ DatabaseHelper.cs
@@ -1,36 +1,104 @@
+using MySql.Data.MySqlClient;
 using System;
+using System.Collections.Generic;
 
 namespace Library_Management_System
 {
-    public class Book
+    public class DatabaseHelper
     {
-        public int Id { get; set; } // Added Id property
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Author { get; set; }
-        public decimal Price { get; set; }
-        public int QtyOnHand { get; set; }
+        private string connectionString;
 
-        public Book() { } // Parameterless constructor for ORM
-
-        public Book(string name, string description, string author, decimal price, int qtyOnHand)
+        public DatabaseHelper()
         {
-            Name = name;
-            Description = description;
-            Author = author;
-            Price = price;
-            QtyOnHand = qtyOnHand;
+            // Update the connection string with your MySQL credentials
+            connectionString = "server=localhost;database=LibraryDB;uid=root;pwd=yourpassword;";
         }
 
-        public override string ToString()
+        // Get all books
+        public List<Book> GetAllBooks()
         {
-            return
-                $"ID: {Id}\n" +
-                $"Name: {Name}\n" +
-                $"Description: {Description}\n" +
-                $"Author: {Author}\n" +
-                $"Price: {Price}\n" +
-                $"Quantity On Hand: {QtyOnHand}";
+            List<Book> books = new List<Book>();
+
+            string query = "SELECT * FROM Books";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                conn.Open();
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Book book = new Book
+                        {
+                            Id = reader.GetInt32("Id"),
+                            Name = reader.GetString("Name"),
+                            Description = reader.IsDBNull(reader.GetOrdinal("Description")) ? "" : reader.GetString("Description"),
+                            Author = reader.IsDBNull(reader.GetOrdinal("Author")) ? "" : reader.GetString("Author"),
+                            Price = reader.GetDecimal("Price"),
+                            QtyOnHand = reader.GetInt32("QtyOnHand")
+                        };
+                        books.Add(book);
+                    }
+                }
+            }
+
+            return books;
+        }
+
+        // Add a new book
+        public void AddBook(Book book)
+        {
+            string query = "INSERT INTO Books (Name, Description, Author, Price, QtyOnHand) VALUES (@Name, @Description, @Author, @Price, @QtyOnHand)";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Name", book.Name);
+                cmd.Parameters.AddWithValue("@Description", book.Description);
+                cmd.Parameters.AddWithValue("@Author", book.Author);
+                cmd.Parameters.AddWithValue("@Price", book.Price);
+                cmd.Parameters.AddWithValue("@QtyOnHand", book.QtyOnHand);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // Remove a book by Id
+        public void RemoveBook(int id)
+        {
+            string query = "DELETE FROM Books WHERE Id = @Id";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // Update a book (Optional)
+        public void UpdateBook(Book book)
+        {
+            string query = "UPDATE Books SET Name=@Name, Description=@Description, Author=@Author, Price=@Price, QtyOnHand=@QtyOnHand WHERE Id=@Id";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Name", book.Name);
+                cmd.Parameters.AddWithValue("@Description", book.Description);
+                cmd.Parameters.AddWithValue("@Author", book.Author);
+                cmd.Parameters.AddWithValue("@Price", book.Price);
+                cmd.Parameters.AddWithValue("@QtyOnHand", book.QtyOnHand);
+                cmd.Parameters.AddWithValue("@Id", book.Id);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
